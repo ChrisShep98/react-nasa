f8b2c39eed9add1f5a8f9f69f8732caa792f5970
ChrisShep98 <shepardchristopher1@gmail.com>
2022-11-22 22:34:58 -0500

    displayed fetched object data on the screen

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import Button from './components/Button'
+// import Button from './components/Button'
 import Date from './components/Date'
 
 function App() {
-  
-  // function getFetch(){
-  //   const choice = document.querySelector('input').value
-
-
-  //   fetch(`https://api.nasa.gov/planetary/apod?api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn&date=${choice}`)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       console.log(data)
-  //     })
-  // }
   return (
     <div className='container mx-auto'>
       <div className='text-center mt-24 border'>
         <h1>Astronomy Picture of the Day</h1>
         <Date />
-        {/* <input className='bg-white hover:bg-gray-100 text-gray-800 font-semibold border border-gray-400 rounded shadow h-8' type='date'></input> */}
-        {/* <Button text={'Get Picture'}/> */}
-        </div>
-      <div>
-        <img src='' alt=''></img>
-        <iframe src='' frameBorder={'0'}></iframe>
       </div>
     </div>
-    // <div className='container mx-auto'>
-    //   <div className='flex justify-center mt-24 border h-40'>
-    //     <h1>Astronomy Picture of the Day</h1>
-    //     <div className='flex justify-center mt-24 border'>
-    //       <input type='date'></input>
-    //       <Button text={'Click for pic!'}/>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
--- src/components/Date.js
+++ src/components/Date.js
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Button from './Button'
 
 const Date = () => {
 
     const inputRef = useRef(null)
+    const [picture, setPicture] = useState('')
+    const [video, setVideo] = useState('')
+    const [title, setTitle] = useState('')
 
     function getFetch(){
 
@@ -11,13 +14,25 @@ const Date = () => {
           .then(res => res.json())
           .then(data => {
             console.log(data)
+            if(data.media_type === 'image'){
+              setPicture(data.hdurl)
+            }else if(data.media_type === 'video'){
+              setVideo(data.url)
+            }
+              setTitle(data.title)
           })
       }
 
   return (
     <div>
         <input className='bg-white hover:bg-gray-100 text-gray-800 font-semibold border border-gray-400 rounded shadow h-8' type='date' ref={inputRef}></input>
-        <Button onClick={getFetch}></Button>
+        <Button text={'Get Picture'} onClick={getFetch}></Button>
+        
+        <div className='w-96 h-96'>
+          <h2>{title}</h2>
+          <img className='object-fit' src={picture} alt=''></img>
+          <iframe src={video} frameBorder={'0'} title={'video'}></iframe>
+        </div>
     </div>
   )
 }
